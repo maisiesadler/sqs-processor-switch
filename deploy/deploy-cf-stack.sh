0a10b5a3eee03c0f2abab4e7540d06179dd20805
Maisie <maisiesadler@hotmail.com>
2022-03-07 19:20:50 +0000

    set function name

--- deploy/deploy-cf-stack.sh
+++ deploy/deploy-cf-stack.sh
@@ -2,13 +2,19 @@
 
 set -e
 
+if [[ -z "${SLACK_LAMBDA_NAME}" ]]; then
+  echo Missing SLACK_LAMBDA_NAME
+  exit 1
+fi
+
 templateFile='./deploy/template.yml'
 
 aws cloudformation deploy \
     --stack-name sqs-processor-lambda-pipeline \
     --template-file $templateFile \
     --capabilities CAPABILITY_IAM \
-    --no-fail-on-empty-changeset
+    --no-fail-on-empty-changeset \
+        SlackLambdaName=$SLACK_LAMBDA_NAME
 
 processorResource=$(aws cloudformation describe-stack-resource \
   --stack-name sqs-processor-lambda-pipeline \
